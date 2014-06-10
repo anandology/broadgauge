66ea8aa1ebc6ea50683ce61c1588d056202446cd
Anand Chitipothu <anandology@gmail.com>
2014-06-11 01:13:40 +0530

    Added context_processor utility to inject vars into template, like in flask.

--- broadgauge/template.py
+++ broadgauge/template.py
@@ -1,4 +1,5 @@
 from jinja2 import Environment, PackageLoader
+import web
 
 env = Environment(loader=PackageLoader('broadgauge', 'templates'))
 
@@ -9,4 +10,22 @@ def render_template(_filename, **kwargs):
     template.
     """
     template = env.get_template(_filename)
-    return template.render(**kwargs)
+    template_vars = _get_injected_vars()
+    template_vars.update(kwargs)
+    return template.render(**template_vars)
+
+def _get_injected_vars():
+    if not 'injected_vars' in web.ctx:
+        d = {}
+        for cp in _context_processors:
+            d.update(cp())
+        web.ctx.injected_vars = d
+    return web.ctx.injected_vars
+
+_context_processors = []
+def context_processor(f):
+    """Utility to inject new variables automatically into the context
+    of a template, like in Flask.
+    """
+    _context_processors.append(f)
+    return f
