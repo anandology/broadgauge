2dcd98f119fa75b46ca38bd3db55317de983a36f
Anand Chitipothu <anandology@gmail.com>
2014-06-30 17:17:59 +0530

    Removed browserid.
    Will go with oauth using github/linkedin.

--- broadgauge/webapp.py
+++ broadgauge/webapp.py
@@ -3,14 +3,11 @@ import web
 import yaml
 
 from .template import render_template, context_processor
-from . import browserid, account
 
 web.config.debug = False
 
 urls = (
     "/", "home",
-    "/auth/login", "login",
-    "/auth/logout", "logout",
 )
 app = web.application(urls, globals())
 
@@ -18,26 +15,12 @@ app = web.application(urls, globals())
 def inject_user():
     return {
         'user': account.get_current_user(),
-        
         'site_title': web.config.get('site_title', 'Broad Gauge')
     }
 
 class home:
     def GET(self):
         return render_template("home.html")
-
-class login:
-    def POST(self):
-        i = web.input("assertion")
-        email = browserid.verify_assertion(i.assertion)
-        if email:
-            account.set_login_cookie(email)
-        return "ok"
-
-class logout:
-    def POST(self):
-        account.logout()
-        return "ok"
 
 def load_config(configfile):
     web.config.update(yaml.load(open(configfile)))
