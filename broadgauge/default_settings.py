3354bbb5e811092cacda36ed7b933810c78b8113
Anand Chitipothu <anandology@gmail.com>
2014-07-08 10:29:14 +0530

    Added default config

---
+++ broadgauge/default_settings.py
@@ -0,0 +1,9 @@
+
+secret_key = "MaXvbXjgqTPRxJrnpZKl"
+
+site_title = "Python Express"
+
+github_client_id = 'f6adcb4752f5c0738aa4'
+github_client_secret = '4f8b629504f059001c8d823f1710b30c73333354'
+
+db_parameters = dict(dbn='postgres', db='pythonexpress')
--- broadgauge/main.py
+++ broadgauge/main.py
@@ -1,7 +1,15 @@
-import web
 import os
+import sys
+import web
+import yaml
+from . import default_settings
 
-def read_config_from_env():
+def load_default_config():
+    # take all vars defined in default_config
+    config = dict((k, v) for k, v in default_settings.__dict__.items() if not k.startswith("_"))
+    web.config.update(config)
+
+def load_config_from_env():
     keys = [
         'SITE_TITLE',
         'GITHUB_CLIENT_ID',
@@ -14,13 +22,26 @@ def read_config_from_env():
         if k in os.environ:
             web.config[k.lower()] = os.environ[k]
 
-
-read_config_from_env()
+load_default_config()
+load_config_from_env()
 from . import webapp
 application = webapp.app.wsgifunc()
 
 # Heroku doesn't handle static files, use StaticMiddleware.
 application = web.httpserver.StaticMiddleware(application)
 
+
+def load_config_from_file(configfile):
+    web.config.update(yaml.load(open(configfile)))
+
+def main():
+    if "--config" in sys.argv:
+        index = sys.argv.index("--config")
+        configfile = sys.argv[index+1]
+        sys.argv = sys.argv[:index] + sys.argv[index+2:]
+        load_config_from_file(configfile)
+
+    webapp.app.run()
+
 if __name__ == '__main__':
-    webapp.main()
+    main()
